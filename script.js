44c88f8c0f76a1b85a4a289d1ee68947bc03f67c
Daniel Papp <papp.daniel.gabor@gmail.com>
2022-07-28 21:37:43 +0200

    Upgrade grid

--- script.js
+++ script.js
@@ -2,22 +2,32 @@
 
 // createGrid: creates a size x size grid for the pixels
 
-function createGrid(size) {
+// function createGrid(size) {
+//     const container = document.getElementById('container');
+//     for (let i = 0; i < size*size; i++) {
+//         const pixel = document.createElement('div');
+//         pixel.classList.add('pixel');
+//         container.appendChild(pixel);
+//     }
+// }
+
+
+function createFlexGrid(size) {
     const container = document.getElementById('container');
-    // const pixel = document.createElement('div');
-    // pixel.classList.add('pixel');
-    for (let i = 0; i < size*size; i++) {
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        container.appendChild(pixel);
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('grid-row');
+        container.appendChild(row);
+        for (let j = 0; j < size; j++) {
+            const pixel = document.createElement('div');
+            pixel.classList.add('pixel');
+            row.appendChild(pixel);
+        }
     }
 }
-
-
-
 
 
 // ------ Calling Functions ------
 
 const size = 16;
-createGrid(size);
+createFlexGrid(size);
